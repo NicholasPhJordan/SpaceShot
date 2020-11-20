6dcc17f60c245d30af64e6fad95ca442b46474cd
Nicholas Jordan <NicholasPhJordan@gmail.com>
2020-11-20 10:52:10 -0600

    Added what i thought I'll need from MathForGames Game class

--- MathForGames/Game.cs
+++ MathForGames/Game.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
+using MathLibrary;
+using Raylib_cs;
 
 namespace MathForGames
 {
     class Game
     {
         private static bool _gameOver = false;
+        private static Scene[] _scenes;
+        private static int _currentSceneIndex;
 
         //Static function used to set game over without an instance of game.
         public static void SetGameOver(bool value)
@@ -15,31 +20,131 @@ namespace MathForGames
             _gameOver = value;
         }
 
+        public static int CurrentSceneIndex
+        {
+            get
+            {
+                return _currentSceneIndex;
+            }
+        }
+
+        public static Scene GetScene(int index)
+        {
+            return _scenes[index];
+        }
+
+        public static Scene GetCurrentScene()
+        {
+            return _scenes[_currentSceneIndex];
+        }
+
+        public static int AddScene(Scene scene)
+        {
+            Scene[] tempArray = new Scene[_scenes.Length + 1];
+
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                tempArray[i] = _scenes[i];
+            }
+
+            int index = _scenes.Length;
+            tempArray[index] = scene;
+            _scenes = tempArray;
+
+            return index;
+        }
+
+        public static bool RemoveScene(Scene scene)
+        {
+            if (scene == null)
+            {
+                return false;
+            }
+
+            bool sceneRemoved = false;
+
+            Scene[] tempArray = new Scene[_scenes.Length - 1];
+
+            int j = 0;
+            for (int i = 0; i < _scenes.Length; i++)
+            {
+                if (tempArray[i] != scene)
+                {
+                    tempArray[j] = _scenes[i];
+                    j++;
+                }
+                else
+                {
+                    sceneRemoved = true;
+                }
+            }
+
+            if (sceneRemoved)
+                _scenes = tempArray;
+
+            return sceneRemoved;
+        }
+
+        public static void SetCurrentScene(int index)
+        {
+            if (index < 0 || index >= _scenes.Length)
+                return;
+
+            if (_scenes[_currentSceneIndex].Started)
+                _scenes[_currentSceneIndex].Start();
+
+            _currentSceneIndex = index;
+        }
+
+        public Game()
+        {
+            _scenes = new Scene[0];
+        }
+
 
         //Called when the game begins. Use this for initialization.
         public void Start()
         {
+            //Creates a new window for raylib
+            Raylib.InitWindow(1024, 760, "Space Shot");
+            Raylib.SetTargetFPS(60);
 
+            //Set up console window
+            Console.CursorVisible = false;
+            Console.Title = "Space Shot";
+
+            //Create a new scene for our actors to exist in
+            Scene scene1 = new Scene();
         }
 
 
         //Called every frame.
-        public void Update()
+        public void Update(float deltaTime)
         {
+            if (!_scenes[_currentSceneIndex].Started)
+                _scenes[_currentSceneIndex].Start();
 
+            _scenes[_currentSceneIndex].Update(deltaTime);
         }
 
         //Used to display objects and other info on the screen.
         public void Draw()
         {
+            Raylib.BeginDrawing();
 
+            Raylib.ClearBackground(Color.BLACK);
+            Console.Clear();
+            _scenes[_currentSceneIndex].Draw();
+
+            Raylib.EndDrawing();
         }
 
 
         //Called when the game ends.
         public void End()
         {
-
+            if (_scenes[_currentSceneIndex].Started)
+                _scenes[_currentSceneIndex].End();
         }
 
 
@@ -48,10 +153,13 @@ namespace MathForGames
         {
             Start();
 
-            while(!_gameOver)
+            while (!_gameOver && !Raylib.WindowShouldClose())
             {
-                Update();
+                float deltaTime = Raylib.GetFrameTime();
+                Update(deltaTime);
                 Draw();
+                while (Console.KeyAvailable)
+                    Console.ReadKey(true);
             }
 
             End();
